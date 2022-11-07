ec32e72b5a618d6cbb5c8ce43d6655bd6e4a12ad
syedmunib616 <syedmunibudden@gmail.com>
2022-11-07 14:56:29 +0500

    secound commit

---
+++ lib/translation/locale_keys.g.dart
@@ -0,0 +1,38 @@
+// // DO NOT EDIT. This is code generated via package:easy_localization/generate.dart
+
+
+abstract class LocaleKeys {
+
+  static const firstData = 'firstData';
+  static const Seconddata = 'Seconddata';
+  static const back = 'back';
+  static const Next = 'Next';
+  static const Changelang = 'Changelang';
+  static const thierddata = 'thierddata';
+
+}
+
+//airpods)
+abstract class TextStrings {
+  static const Name	= "Name";
+  static const Company_name	= "Company Name";
+  static const Email	= "Email";
+  static const Phone_No	= "Phone No";
+  static const Password	= "Password";
+  static const ConformPassword	= "Conform Password";
+  static const Sgin_Up= "Sgin Up";
+
+
+  static const Create_Account	=	"Create_Account";
+  static const Done ='Done';
+  static const Search ='Search';
+  static const Cancel ='Cancel';
+  static const OR='OR';
+  static const Success='Success!';
+  static const Sign_In='Sign In';
+  static const Remember_Me='Remember Me';
+
+}
+
+
+
